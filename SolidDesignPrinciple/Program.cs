cd7fd492b02907833e015cd7f363c40faa253fd4
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-08-02 21:55:58 +0530

    [Soubarnika]Implementaion of solid design principle

---
+++ SolidDesignPrinciple/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SolidDesignPrinciple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Implementaion of solid design principle!");
+        }
+    }
+}
