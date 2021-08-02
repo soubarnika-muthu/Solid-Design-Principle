6bd676fd15c8a0f878e9ec96e376a886bdbfe732
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-08-02 22:27:40 +0530

    [Soubarnika]Execution of Open closed principle

---
+++ OpenClosePrinciple/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OpenClosePrinciple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Implementaion of open close principle");
+            Shape shape = new Rectangle();
+            
+            shape.Area();
+
+        }
+    }
+}
