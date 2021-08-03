f7b9e75317b06ce110e96dd8e5dba91431cc357e
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-08-03 19:26:08 +0530

    [Soubarnika]Execution of  LiskovSubstitutionPrinciple

--- LiskovSubstitutionPrinciple/Program.cs
+++ LiskovSubstitutionPrinciple/Program.cs
@@ -2,11 +2,16 @@
 
 namespace LiskovSubstitutionPrinciple
 {
+    //LSP states that the derived classes should be perfectly substitutable for their base classes.
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Implementaion of LiskovSubstitutionPrinciple");
+            Shape shape = new Rectangle();
+            Console.WriteLine(shape.Area());
+            shape = new Circle();
+            Console.WriteLine(shape.Area());
         }
     }
 }
